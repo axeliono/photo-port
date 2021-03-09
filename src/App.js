161949e5cb27ea0744775d685a03d31ba1d45dea
axeliono <69375446+axeliono@users.noreply.github.com>
2021-03-09 10:02:24 -0600

    contact form state/submission control

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -22,10 +23,11 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      ></Nav>
+      />
       <main>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        <ContactForm/>
+        <Gallery currentCategory={currentCategory}/>
+        <About/>
       </main>
     </div>
   );
